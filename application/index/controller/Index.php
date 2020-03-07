fb3ccf87e5e81e51981e0970dca6aa7c558233e0
niping <1162323144@qq.com>
2020-03-07 12:46:46 +0800

    测试二维数组

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,4 +7,17 @@ class Index
     {
         echo '我是测试数据123';
     }
+    public function getInfo()
+    {
+        return {
+            type: 8002,
+            a: '数据1',
+            b: '数据2',
+            c: '数据3',
+            d: {
+                a: '二维数据1',
+                b: '二维数据2'
+            }
+        }
+    }
 }
